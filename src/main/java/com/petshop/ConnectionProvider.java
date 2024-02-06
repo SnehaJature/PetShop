a3bcbb7d4a3fea7629aeb733b57a4832d27fa59b
royalpatilsp <sudarshan007patil@gmail.com>
2024-02-06 10:38:07 +0530

    Index Price Upates

--- src/main/java/com/petshop/ConnectionProvider.java
+++ src/main/java/com/petshop/ConnectionProvider.java
@@ -1,6 +1,24 @@
 package com.petshop;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class ConnectionProvider {
+	
+	private static Connection con;
+	
+	public static Connection con() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/petcare", "root", "root");
+		    return con();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			
+		}
+		return con();
+	}
 
 	
 }
